c000c6db83a0f00d9dfe78be6076c078ce5b9552
ivram17 <ivram17@hotmail.com>
2022-07-28 17:52:18 -0300

    tp web

---
+++ Js/stylejs.js
@@ -0,0 +1,7 @@
+function accesoProv(){
+    let usuario;
+    usuario = prompt ("Ingrese nombre de Usuario");
+    prompt (" USUARIO:  " +   usuario + ".   Ingrese PASSWORD ");
+    window.location.href = "./pages/proveedores.html";
+
+}
